bc60f54f0b1ada1908d6c1b80ff3be78e3d75184
JeanDes-Code <78590821+JeanDes-Code@users.noreply.github.com>
2022-11-11 15:08:04 +0100

    Implementing Context

---
+++ client/src/contract/index.js
@@ -0,0 +1,4 @@
+import contract from './AVAXGods.json';
+export const ADDRESS = '0x4Bc91a244125FD925F93268C176d67Adc291BdE9';
+
+export const { abi: ABI } = contract;
--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { PageHOC } from '../components';
+import { useGlobalContext } from '../context';
+import { PageHOC, CustomInput, CustomButton } from '../components';
 
 const Home = () => {
+  const { contract, walletAddress } = useGlobalContext();
+  const [playerName, setPlayerName] = useState('');
+
   return (
-    <div>
-     <h1 className='text-white text-xl'> Hello From Home</h1>
+    <div className="flex flex-col">
+      <CustomInput
+        label="Name"
+        placeholder="Enter your player name"
+        value={playerName}
+        handleValueChange={setPlayerName}
+      />
+
+      <CustomButton title="Register" handleClick={() => {}} restStyles="mt-6" />
     </div>
-  )
+  );
 };
 
 export default PageHOC(
   Home,
-<>
-Welcome to Avax Gods <br/> a Web3 NFT Card Game
-</>,
-<>
-Connect your wallet to start playing <br/> the ultimate Web3 Battle Card Game 
-</>
+  <>
+    Welcome to Avax Gods <br /> a Web3 NFT Card Game
+  </>,
+  <>
+    Connect your wallet to start playing <br /> the ultimate Web3 Battle Card
+    Game
+  </>,
 );
