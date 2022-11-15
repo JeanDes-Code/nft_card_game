d15dc774d77334a56de5b245067d0bee2c148a99
JeanDes-Code <78590821+JeanDes-Code@users.noreply.github.com>
2022-11-15 17:01:05 +0100

    Un pas en avant trois pas en arrière.

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -4,8 +4,28 @@ import { useGlobalContext } from '../context';
 import { PageHOC, CustomInput, CustomButton } from '../components';
 
 const Home = () => {
-  const { contract, walletAddress } = useGlobalContext();
+  const { contract, walletAddress, setShowAlert } = useGlobalContext();
   const [playerName, setPlayerName] = useState('');
+
+  const handleClick = async () => {
+    try {
+      const playerExists = await contract.isPlayer(walletAddress);
+
+      if (!playerExists) {
+        await contract.registerPlayer(playerName, playerName, {
+          gasLimit: 500000,
+        });
+
+        setShowAlert({
+          status: 'true',
+          type: 'info',
+          message: `${playerName} is being summoned!`,
+        });
+      }
+    } catch (error) {
+      alert(error);
+    }
+  };
 
   return (
     <div className="flex flex-col">
@@ -16,7 +36,11 @@ const Home = () => {
         handleValueChange={setPlayerName}
       />
 
-      <CustomButton title="Register" handleClick={() => {}} restStyles="mt-6" />
+      <CustomButton
+        title="Register"
+        handleClick={handleClick}
+        restStyles="mt-6"
+      />
     </div>
   );
 };
